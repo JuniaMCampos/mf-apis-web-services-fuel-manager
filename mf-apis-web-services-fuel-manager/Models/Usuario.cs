7a2b513c3536a652bf1da93b1fa194b1bf02f7af
JuniaMCampos <juniacamposjc@gmail.com>
2024-02-16 20:35:59 -0300

    model usuario

---
+++ mf-apis-web-services-fuel-manager/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        [Display(Name = "Administrador")]
+        Administrador,
+        [Display(Name = "Usuario")]
+        Usuario
+    }
+}
