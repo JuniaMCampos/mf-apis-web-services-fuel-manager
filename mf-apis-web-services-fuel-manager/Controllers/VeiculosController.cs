06444e18e3778f84eacd1b1102e8497e085efdee
JuniaMCampos <juniacamposjc@gmail.com>
2024-02-15 21:40:35 -0300

    API Veiculos

---
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -0,0 +1,90 @@
+﻿using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        //configuração do banco de dados
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //rota de index para receber todos os carros cadastrados
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+
+            return Ok(model);
+        }
+
+        //para criar um novo veiculo
+        [HttpPost]
+
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if(model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
+            {
+                return BadRequest(new { message = "Ano de Fabricação e ano do Modelo são obrigatório e devem ser maiores que zero" });
+            }
+
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new {id = model.Id}, model);
+        }
+
+        //recuperação de dados
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) NotFound();
+
+            return Ok(model);
+        }
+
+        //alteração de dados
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult> Update(int id, Veiculo model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modeloDb = await _context.Veiculos.AsNoTracking()
+                .FirstOrDefaultAsync (c => c.Id == id);
+
+            if (modeloDb == null) return NotFound();
+
+            _context.Veiculos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //deletar dados
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Veiculos.FindAsync(id);
+            if(model == null) NotFound();
+
+            _context.Veiculos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
