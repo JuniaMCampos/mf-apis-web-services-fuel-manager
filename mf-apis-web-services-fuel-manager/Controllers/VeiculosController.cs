31028e593d864264575bef4bf2969a1f247d45f1
JuniaMCampos <juniacamposjc@gmail.com>
2024-02-17 18:27:43 -0300

    adicionando usuario a veiculos

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -48,6 +48,7 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         public async Task<ActionResult> GetById(int id)
         {
             var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t=>t.Usuario)
                 .Include(t => t.Consumos)
                 .FirstOrDefaultAsync(c => c.Id == id);
 
@@ -95,6 +96,34 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink() , rel: "self", metodo: "GET"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "Delete"));
+        }
+
+        //rotas para adicionar um usuario a um determinado veiculo
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int id, VeiculoUsuario model)
+        {
+            if (id != model.VeiculoId) return BadRequest();
+
+            _context.VeiculoUsuarios.Add(model);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.VeiculoId }, model);
+        }
+
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int id, int usuarioId)
+        {
+            var model = await _context.VeiculoUsuarios
+                .Where(c => c.VeiculoId == id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if (model == null) return NotFound();
+
+            _context.VeiculoUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
